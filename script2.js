96c0b0e09df52c3bd93b63810f08b6f4a1c04f92
Jaldson Arthur <126914358+JaldsonArthur17@users.noreply.github.com>
2024-10-14 17:52:29 -0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,8 +1,11 @@
-// Reproduzir som de início
-const startupSound = new Audio('iniciar.mp3'); // Som ao iniciar
-startupSound.play();
+// Esperar o carregamento completo da página antes de reproduzir o som
+window.onload = () => {
+    // Reproduzir som de início
+    const startupSound = new Audio('iniciar.mp3'); // Som ao iniciar
+    startupSound.play();
 
-// Redirecionar após 3 segundos para a tela de chat
-setTimeout(() => {
-    window.location.href = 'index.html'; // Redireciona para a tela de chat
-}, 3000);
+    // Redirecionar após 3 segundos para a tela de chat
+    setTimeout(() => {
+        window.location.href = 'index.html'; // Redireciona para a tela de chat
+    }, 3000);
+};
