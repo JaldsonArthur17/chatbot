869a41ef528716e7c6388eae5e1cc2d24023c5ff
Jaldson Arthur <126914358+JaldsonArthur17@users.noreply.github.com>
2024-10-14 17:44:18 -0300

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,8 @@
+// Reproduzir som de início
+const startupSound = new Audio('iniciar.mp3'); // Som ao iniciar
+startupSound.play();
+
+// Redirecionar após 3 segundos para a tela de chat
+setTimeout(() => {
+    window.location.href = 'index.html'; // Redireciona para a tela de chat
+}, 3000);
